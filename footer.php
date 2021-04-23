ef60529fd237a25380bf4cc6d887ee73d2cc404b
niko <mynikoxp@gmail.com>
2021-04-24 07:57:06 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
             Copyright © 2015-<?php echo date('Y');?> All Rights Reserved </br>
         </p>
         <p  class="text-white  m-1 p-1 h6">
-            Powered by <a href="https://you2php.github.io/" target="_blank" class="text-white font-italic">YOU2PHP</a> 
+            Powered by <a href="https://y2p.github.io/" target="_blank" class="text-white font-italic">y2p</a> 
             <a href="./embed/upgrade.php" target="_blank" class="text-white ml-2">检查更新</a>
         </p>
     </footer>
